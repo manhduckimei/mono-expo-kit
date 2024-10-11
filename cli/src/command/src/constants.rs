1603d36d26e92bda3baec95948e0356578c731cc
manhduckimei <ducnmanh@kimei.vn>
2024-10-11 13:33:33 +0700

    wip 11/10

--- cli/src/command/src/constants.rs
+++ cli/src/command/src/constants.rs
@@ -8,7 +8,7 @@ pub struct ComponentData {
 
 // Handlebars template for the component
 pub const COMPONENT_TEMPLATE: &str = r#"import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function {{name}}() {
   return (
@@ -17,4 +17,13 @@ export default function {{name}}() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  text: {
+    fontSize: 20
+  }
+})
 "#;
